74536d4deb87102302a0149f2b04064fd12f91fe
sinisa94 <sinisa.m1994@gmail.com>
2024-07-13 23:31:59 +0200

    fixup

--- export.sh
+++ export.sh
@@ -4,4 +4,5 @@ sudo cp ~/.Xresources ~/dotfiles/.Xresources
 sudo cp ~/.emacs.d/init.el ~/dotfiles/init.el
 sudo cp /etc/i3status.conf ~/dotfiles/i3status.conf
 sudo cp ~/.xinitrc ~/dotfiles/xinitrc
+sudo cp ~/.xinputrc ~/dotfiles/xinputrc
 echo "Exported configs to -> ~/dotfiles/"
--- import.sh
+++ import.sh
@@ -3,4 +3,5 @@ sudo cp ~/.config/i3/config ~/dotfiles
 sudo cp ~/.Xresources ~/dotfiles/.Xresources
 sudo cp ~/.emacs.d/init.el ~/dotfiles/init.el
 sudo cp /etc/i3status.conf ~/dotfiles/i3status.conf
+
 echo "Imported configs from -> ~/dotfiles/"
