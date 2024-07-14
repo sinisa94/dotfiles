78ffe48a7d6440d301b2c8b474c74fbf53c9578b
Sinisa <sinisa.m1994@gmail.com>
2024-07-14 15:00:40 +0200

    bash cosmetics

--- import.sh
+++ import.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
-sudo cp ~/.config/i3/config ~/dotfiles
-sudo cp ~/.Xresources ~/dotfiles/.Xresources
-sudo cp ~/.emacs.d/init.el ~/dotfiles/init.el
-sudo cp /etc/i3status.conf ~/dotfiles/i3status.conf
-
+sudo cp ~/dotfiles/xinitrc ~/.xinitrc 
+sudo cp ~/dotfiles/.inputrc ~/.inputrc
+sudo cp ~/dotfiles/.Xresources ~/.Xresources 
+sudo cp ~/dotfiles/config ~/.config/i3/config 
+sudo cp ~/dotfiles/i3status.conf /etc/i3status.conf 
+sudo cp ~/dotfiles/init.el ~/.emacs.d/init.el 
+sudo cp ~/dotfiles/hhkb-i3.sh ~/hhkb-i3.sh
+sudo cp ~/dotfiles/monitors.sh ~/monitors.sh 
+sudo chmod +x ~/hhkb-i3.sh 
 echo "Imported configs from -> ~/dotfiles/"
