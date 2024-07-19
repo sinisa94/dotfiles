5484a963ee0cb387651f1ed754f72d1d711d1c8a
Sinisa <sinisa.m1994@gmail.com>
2024-07-19 22:00:33 +0200

    dwm init

---
+++ autostart.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# NetworkManager is the most popular way to manage wireless networks on Linux,
+# and nm-applet is a desktop environment-independent system tray GUI for it.
+exec nm-applet &
+exec pasystray &
+exec firewalld &
+exec firewall-applet &
+exec dwmblocks &
+exec firefox &
+exec solaar -w hide &
+exec ~/monitors.sh
+exec ~/hhkb-i3.sh
