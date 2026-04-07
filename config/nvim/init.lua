af3b4861ebc8ac4f5ef2c8bec03695f2cf38238d
Sinisa <sinisa.m1994@gmail.com>
2026-04-07 11:43:58 +0200

    fix double binds

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -127,7 +127,6 @@ vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left wind
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "Open git" })
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
@@ -182,7 +181,7 @@ require("lazy").setup({
 		"tpope/vim-fugitive",
 		config = function()
 			-- Optional: Set keymaps or additional config
-			vim.keymap.set("n", "<leader>gh", ":Git<cr>", { desc = "Git status" })
+			vim.keymap.set("n", "<leader>gs", ":Git<cr>", { desc = "Git status" })
 		end,
 	},
 	{
